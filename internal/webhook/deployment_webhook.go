3f8bd3472243ea2e9e0db2d12917fd253b052549
Ekaterina Kazakova <eromanova@mirantis.com>
2024-08-06 11:28:52 +0400

    Apply default deployment conifguration by the admission controller
    HMC-95

--- internal/webhook/deployment_webhook.go
+++ internal/webhook/deployment_webhook.go
@@ -16,9 +16,13 @@ package webhook // nolint:dupl
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Mirantis/hmc/api/v1alpha1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -59,6 +63,25 @@ func (*DeploymentValidator) ValidateDelete(_ context.Context, _ runtime.Object) 
 }
 
 // Default implements webhook.Defaulter so a webhook will be registered for the type.
-func (*DeploymentValidator) Default(_ context.Context, _ runtime.Object) error {
+func (in *DeploymentValidator) Default(ctx context.Context, obj runtime.Object) error {
+	deployment, ok := obj.(*v1alpha1.Deployment)
+	if !ok {
+		return apierrors.NewBadRequest(fmt.Sprintf("expected Deployment but got a %T", obj))
+	}
+	template := &v1alpha1.Template{}
+	templateRef := types.NamespacedName{Name: deployment.Spec.Template, Namespace: v1alpha1.TemplatesNamespace}
+	if err := in.Get(ctx, templateRef, template); err != nil {
+		return err
+	}
+	applyDefaultDeploymentConfiguration(deployment, template)
 	return nil
+}
+
+func applyDefaultDeploymentConfiguration(deployment *v1alpha1.Deployment, template *v1alpha1.Template) {
+	if deployment.Spec.Config != nil || template.Status.Config == nil {
+		// Only apply defaults when there's no configuration provided
+		return
+	}
+	deployment.Spec.DryRun = true
+	deployment.Spec.Config = &apiextensionsv1.JSON{Raw: template.Status.Config.Raw}
 }
