wahabmk/kcm

Install Sveltos as a Provider Template

internal/controller/management_controller.go
@@ -100,8 +100,10 @@ func (r *ManagementReconciler) Update(ctx context.Context, management *hmc.Manag
 
 	components := wrappedComponents(management)
 	for _, component := range components {
+		fmt.Printf("\n>>>>>>>>>>>>>>>>>>>>>>>>> component.Template = %s >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n", component.Template)
 		template := &hmc.Template{}
 		err := r.Get(ctx, types.NamespacedName{
+			// WAHAB: 3
 			Namespace: r.SystemNamespace,
 			Name:      component.Template,
 		}, template)
@@ -118,8 +120,13 @@ func (r *ManagementReconciler) Update(ctx context.Context, management *hmc.Manag
 			continue
 		}
 
-		_, _, err = helm.ReconcileHelmRelease(ctx, r.Client, component.HelmReleaseName(), r.SystemNamespace, component.Config,
-			nil, template.Status.ChartRef, defaultReconcileInterval, component.dependsOn)
+		_, _, err = helm.ReconcileHelmRelease2(ctx, r.Client, component.HelmReleaseName(), r.SystemNamespace, helm.ReconcileHelmReleaseOpts{
+			Values:          component.Config,
+			ChartRef:        template.Status.ChartRef,
+			DependsOn:       component.dependsOn,
+			TargetNamespace: component.targetNamespace,
+			CreateNamespace: component.createNamespace,
+		})
 		if err != nil {
 			errMsg := fmt.Sprintf("error reconciling HelmRelease %s/%s: %s", r.SystemNamespace, component.Template, err)
 			updateComponentsStatus(detectedComponents, &detectedProviders, component.Template, template.Status, errMsg)
@@ -213,19 +220,31 @@ func (r *ManagementReconciler) removeHelmRepositories(ctx context.Context, opts 
 
 type component struct {
 	hmc.Component
+
 	// helm release dependencies
-	dependsOn []meta.NamespacedObjectReference
+	dependsOn       []meta.NamespacedObjectReference
+	targetNamespace string
+	createNamespace bool
 }
 
 func wrappedComponents(mgmt *hmc.Management) (components []component) {
 	if mgmt.Spec.Core == nil {
 		return
 	}
+
 	components = append(components, component{Component: mgmt.Spec.Core.HMC})
 	components = append(components, component{Component: mgmt.Spec.Core.CAPI, dependsOn: []meta.NamespacedObjectReference{{Name: mgmt.Spec.Core.HMC.Template}}})
-	for provider := range mgmt.Spec.Providers {
-		components = append(components, component{Component: mgmt.Spec.Providers[provider], dependsOn: []meta.NamespacedObjectReference{{Name: mgmt.Spec.Core.CAPI.Template}}})
+
+	for i := range mgmt.Spec.Providers {
+		c := component{Component: mgmt.Spec.Providers[i], dependsOn: []meta.NamespacedObjectReference{{Name: mgmt.Spec.Core.CAPI.Template}}}
+
+		if mgmt.Spec.Providers[i].Template == hmc.ProviderSveltosName {
+			c.targetNamespace = hmc.ProviderSveltosTargetNamespace
+			c.createNamespace = hmc.ProviderSveltosCreateNamespace
+		}
+		components = append(components, c)
 	}
+
 	return
 }
 

internal/controller/release_controller.go
@@ -128,7 +128,10 @@ func (p *Poller) ensureManagement(ctx context.Context) error {
 		if !apierrors.IsNotFound(err) {
 			return fmt.Errorf("failed to get %s Management object", hmc.ManagementName)
 		}
-		mgmtObj.Spec.SetProvidersDefaults()
+
+		if err := mgmtObj.Spec.SetProvidersDefaults(); err != nil {
+			return err
+		}
 
 		getter := helm.NewMemoryRESTClientGetter(p.Config, p.RESTMapper())
 		actionConfig := new(action.Configuration)
@@ -164,6 +167,7 @@ func (p *Poller) ensureManagement(ctx context.Context) error {
 			Raw: rawConfig,
 		}
 
+		// WAHAB: 2
 		err = p.Create(ctx, mgmtObj)
 		if err != nil {
 			return fmt.Errorf("failed to create %s Management object: %s", hmc.ManagementName, err)
@@ -190,7 +194,7 @@ func (p *Poller) reconcileDefaultHelmRepo(ctx context.Context) error {
 		helmRepo.Spec = sourcev1.HelmRepositorySpec{
 			Type:     p.DefaultRepoType,
 			URL:      p.DefaultRegistryURL,
-			Interval: metav1.Duration{Duration: defaultReconcileInterval},
+			Interval: metav1.Duration{Duration: helm.DefaultReconcileInterval},
 			Insecure: p.InsecureRegistry,
 		}
 		if p.RegistryCredentialsSecret != "" {
@@ -234,7 +238,7 @@ func (p *Poller) reconcileHMCTemplates(ctx context.Context) error {
 				Kind: sourcev1.HelmRepositoryKind,
 				Name: defaultRepoName,
 			},
-			Interval: metav1.Duration{Duration: defaultReconcileInterval},
+			Interval: metav1.Duration{Duration: helm.DefaultReconcileInterval},
 		}
 		return nil
 	})
@@ -255,7 +259,9 @@ func (p *Poller) reconcileHMCTemplates(ctx context.Context) error {
 		Name:      helmChart.Name,
 		Namespace: helmChart.Namespace,
 	}
-	_, operation, err = helm.ReconcileHelmRelease(ctx, p.Client, hmcTemplatesReleaseName, p.SystemNamespace, nil, nil, chartRef, defaultReconcileInterval, nil)
+	_, operation, err = helm.ReconcileHelmRelease2(ctx, p.Client, hmcTemplatesReleaseName, p.SystemNamespace, helm.ReconcileHelmReleaseOpts{
+		ChartRef: chartRef,
+	})
 	if err != nil {
 		return err
 	}

internal/helm/release.go
@@ -29,45 +29,64 @@ import (
 	hmc "github.com/Mirantis/hmc/api/v1alpha1"
 )
 
-func ReconcileHelmRelease(
-	ctx context.Context,
+const (
+	DefaultReconcileInterval = 10 * time.Minute
+)
+
+type ReconcileHelmReleaseOpts struct {
+	Values            *apiextensionsv1.JSON
+	OwnerReference    *metav1.OwnerReference
+	ChartRef          *hcv2.CrossNamespaceSourceReference
+	ReconcileInterval *time.Duration
+	DependsOn         []meta.NamespacedObjectReference
+	TargetNamespace   string
+	CreateNamespace   bool
+}
+
+func ReconcileHelmRelease2(ctx context.Context,
 	cl client.Client,
 	name string,
 	namespace string,
-	values *apiextensionsv1.JSON,
-	ownerReference *metav1.OwnerReference,
-	chartRef *hcv2.CrossNamespaceSourceReference,
-	reconcileInterval time.Duration,
-	dependsOn []meta.NamespacedObjectReference,
+	opts ReconcileHelmReleaseOpts,
 ) (*hcv2.HelmRelease, controllerutil.OperationResult, error) {
-	helmRelease := &hcv2.HelmRelease{
+	hr := &hcv2.HelmRelease{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: namespace,
 		},
 	}
 
-	operation, err := ctrl.CreateOrUpdate(ctx, cl, helmRelease, func() error {
-		if helmRelease.Labels == nil {
-			helmRelease.Labels = make(map[string]string)
+	operation, err := ctrl.CreateOrUpdate(ctx, cl, hr, func() error {
+		if hr.Labels == nil {
+			hr.Labels = make(map[string]string)
 		}
-		helmRelease.Labels[hmc.HMCManagedLabelKey] = hmc.HMCManagedLabelValue
-		if ownerReference != nil {
-			helmRelease.OwnerReferences = []metav1.OwnerReference{*ownerReference}
+		hr.Labels[hmc.HMCManagedLabelKey] = hmc.HMCManagedLabelValue
+		if opts.OwnerReference != nil {
+			hr.OwnerReferences = []metav1.OwnerReference{*opts.OwnerReference}
 		}
-		helmRelease.Spec = hcv2.HelmReleaseSpec{
-			ChartRef:    chartRef,
-			Interval:    metav1.Duration{Duration: reconcileInterval},
-			ReleaseName: name,
-			Values:      values,
-			DependsOn:   dependsOn,
+		hr.Spec = hcv2.HelmReleaseSpec{
+			ChartRef: opts.ChartRef,
+			Interval: metav1.Duration{Duration: func() time.Duration {
+				if opts.ReconcileInterval != nil {
+					return *opts.ReconcileInterval
+				}
+				return DefaultReconcileInterval
+			}()},
+			ReleaseName:     name,
+			Values:          opts.Values,
+			DependsOn:       opts.DependsOn,
+			TargetNamespace: opts.TargetNamespace,
+			Install: &hcv2.Install{
+				CreateNamespace: opts.CreateNamespace,
+			},
 		}
 		return nil
 	})
 	if err != nil {
 		return nil, operation, err
 	}
-	return helmRelease, operation, nil
+
+	return hr, operation, nil
 }
 
 func DeleteHelmRelease(ctx context.Context, cl client.Client, name string, namespace string) error {

Mirantis/hmc

internal/controller/management_controller.go
@@ -117,8 +117,13 @@ func (r *ManagementReconciler) Update(ctx context.Context, management *hmc.Manag
 			continue
 		}
 
-		_, _, err = helm.ReconcileHelmRelease(ctx, r.Client, component.HelmReleaseName(), r.SystemNamespace, component.Config,
-			nil, template.Status.ChartRef, defaultReconcileInterval, component.dependsOn)
+		_, _, err = helm.ReconcileHelmRelease(ctx, r.Client, component.HelmReleaseName(), r.SystemNamespace, helm.ReconcileHelmReleaseOpts{
+			Values:          component.Config,
+			ChartRef:        template.Status.ChartRef,
+			DependsOn:       component.dependsOn,
+			TargetNamespace: component.targetNamespace,
+			CreateNamespace: component.createNamespace,
+		})
 		if err != nil {
 			errMsg := fmt.Sprintf("error reconciling HelmRelease %s/%s: %s", r.SystemNamespace, component.Template, err)
 			updateComponentsStatus(detectedComponents, &detectedProviders, component.Template, template.Status, errMsg)
@@ -213,18 +218,29 @@ func (r *ManagementReconciler) removeHelmRepositories(ctx context.Context, opts 
 type component struct {
 	hmc.Component
 	// helm release dependencies
-	dependsOn []meta.NamespacedObjectReference
+	dependsOn       []meta.NamespacedObjectReference
+	targetNamespace string
+	createNamespace bool
 }
 
 func wrappedComponents(mgmt *hmc.Management) (components []component) {
 	if mgmt.Spec.Core == nil {
 		return
 	}
+
 	components = append(components, component{Component: mgmt.Spec.Core.HMC})
 	components = append(components, component{Component: mgmt.Spec.Core.CAPI, dependsOn: []meta.NamespacedObjectReference{{Name: mgmt.Spec.Core.HMC.Template}}})
-	for provider := range mgmt.Spec.Providers {
-		components = append(components, component{Component: mgmt.Spec.Providers[provider], dependsOn: []meta.NamespacedObjectReference{{Name: mgmt.Spec.Core.CAPI.Template}}})
+
+	for i := range mgmt.Spec.Providers {
+		c := component{Component: mgmt.Spec.Providers[i], dependsOn: []meta.NamespacedObjectReference{{Name: mgmt.Spec.Core.CAPI.Template}}}
+
+		if mgmt.Spec.Providers[i].Template == hmc.ProviderSveltosName {
+			c.targetNamespace = hmc.ProviderSveltosTargetNamespace
+			c.createNamespace = hmc.ProviderSveltosCreateNamespace
+		}
+		components = append(components, c)
 	}
+
 	return
 }
 

internal/controller/release_controller.go
@@ -128,7 +128,10 @@ func (p *Poller) ensureManagement(ctx context.Context) error {
 		if !apierrors.IsNotFound(err) {
 			return fmt.Errorf("failed to get %s Management object", hmc.ManagementName)
 		}
-		mgmtObj.Spec.SetProvidersDefaults()
+
+		if err := mgmtObj.Spec.SetProvidersDefaults(); err != nil {
+			return err
+		}
 
 		getter := helm.NewMemoryRESTClientGetter(p.Config, p.RESTMapper())
 		actionConfig := new(action.Configuration)
@@ -190,7 +193,7 @@ func (p *Poller) reconcileDefaultHelmRepo(ctx context.Context) error {
 		helmRepo.Spec = sourcev1.HelmRepositorySpec{
 			Type:     p.DefaultRepoType,
 			URL:      p.DefaultRegistryURL,
-			Interval: metav1.Duration{Duration: defaultReconcileInterval},
+			Interval: metav1.Duration{Duration: helm.DefaultReconcileInterval},
 			Insecure: p.InsecureRegistry,
 		}
 		if p.RegistryCredentialsSecret != "" {
@@ -234,7 +237,7 @@ func (p *Poller) reconcileHMCTemplates(ctx context.Context) error {
 				Kind: sourcev1.HelmRepositoryKind,
 				Name: defaultRepoName,
 			},
-			Interval: metav1.Duration{Duration: defaultReconcileInterval},
+			Interval: metav1.Duration{Duration: helm.DefaultReconcileInterval},
 		}
 		return nil
 	})
@@ -250,12 +253,13 @@ func (p *Poller) reconcileHMCTemplates(ctx context.Context) error {
 		return fmt.Errorf("HelmChart %s/%s Artifact is not ready: %w", p.SystemNamespace, p.HMCTemplatesChartName, err)
 	}
 
-	chartRef := &hcv2.CrossNamespaceSourceReference{
-		Kind:      helmChart.Kind,
-		Name:      helmChart.Name,
-		Namespace: helmChart.Namespace,
-	}
-	_, operation, err = helm.ReconcileHelmRelease(ctx, p.Client, hmcTemplatesReleaseName, p.SystemNamespace, nil, nil, chartRef, defaultReconcileInterval, nil)
+	_, operation, err = helm.ReconcileHelmRelease(ctx, p.Client, hmcTemplatesReleaseName, p.SystemNamespace, helm.ReconcileHelmReleaseOpts{
+		ChartRef: &hcv2.CrossNamespaceSourceReference{
+			Kind:      helmChart.Kind,
+			Name:      helmChart.Name,
+			Namespace: helmChart.Namespace,
+		},
+	})
 	if err != nil {
 		return err
 	}

internal/helm/release.go
@@ -29,45 +29,64 @@ import (
 	hmc "github.com/Mirantis/hmc/api/v1alpha1"
 )
 
-func ReconcileHelmRelease(
-	ctx context.Context,
+const (
+	DefaultReconcileInterval = 10 * time.Minute
+)
+
+type ReconcileHelmReleaseOpts struct {
+	Values            *apiextensionsv1.JSON
+	OwnerReference    *metav1.OwnerReference
+	ChartRef          *hcv2.CrossNamespaceSourceReference
+	ReconcileInterval *time.Duration
+	DependsOn         []meta.NamespacedObjectReference
+	TargetNamespace   string
+	CreateNamespace   bool
+}
+
+func ReconcileHelmRelease(ctx context.Context,
 	cl client.Client,
 	name string,
 	namespace string,
-	values *apiextensionsv1.JSON,
-	ownerReference *metav1.OwnerReference,
-	chartRef *hcv2.CrossNamespaceSourceReference,
-	reconcileInterval time.Duration,
-	dependsOn []meta.NamespacedObjectReference,
+	opts ReconcileHelmReleaseOpts,
 ) (*hcv2.HelmRelease, controllerutil.OperationResult, error) {
-	helmRelease := &hcv2.HelmRelease{
+	hr := &hcv2.HelmRelease{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: namespace,
 		},
 	}
 
-	operation, err := ctrl.CreateOrUpdate(ctx, cl, helmRelease, func() error {
-		if helmRelease.Labels == nil {
-			helmRelease.Labels = make(map[string]string)
+	operation, err := ctrl.CreateOrUpdate(ctx, cl, hr, func() error {
+		if hr.Labels == nil {
+			hr.Labels = make(map[string]string)
 		}
-		helmRelease.Labels[hmc.HMCManagedLabelKey] = hmc.HMCManagedLabelValue
-		if ownerReference != nil {
-			helmRelease.OwnerReferences = []metav1.OwnerReference{*ownerReference}
+		hr.Labels[hmc.HMCManagedLabelKey] = hmc.HMCManagedLabelValue
+		if opts.OwnerReference != nil {
+			hr.OwnerReferences = []metav1.OwnerReference{*opts.OwnerReference}
 		}
-		helmRelease.Spec = hcv2.HelmReleaseSpec{
-			ChartRef:    chartRef,
-			Interval:    metav1.Duration{Duration: reconcileInterval},
-			ReleaseName: name,
-			Values:      values,
-			DependsOn:   dependsOn,
+		hr.Spec = hcv2.HelmReleaseSpec{
+			ChartRef: opts.ChartRef,
+			Interval: metav1.Duration{Duration: func() time.Duration {
+				if opts.ReconcileInterval != nil {
+					return *opts.ReconcileInterval
+				}
+				return DefaultReconcileInterval
+			}()},
+			ReleaseName:     name,
+			Values:          opts.Values,
+			DependsOn:       opts.DependsOn,
+			TargetNamespace: opts.TargetNamespace,
+			Install: &hcv2.Install{
+				CreateNamespace: opts.CreateNamespace,
+			},
 		}
 		return nil
 	})
 	if err != nil {
 		return nil, operation, err
 	}
-	return helmRelease, operation, nil
+
+	return hr, operation, nil
 }
 
 func DeleteHelmRelease(ctx context.Context, cl client.Client, name string, namespace string) error {