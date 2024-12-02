510c6df61c96be0bb81f277c6a329a0200e58aa3
Kyle Wuolle <kyle.wuolle@gmail.com>
2024-12-02 12:13:38 -0800

    Add debug logging to ccm check

--- test/e2e/managedcluster/validate_deployed.go
+++ test/e2e/managedcluster/validate_deployed.go
@@ -266,6 +266,8 @@ func validateCCM(ctx context.Context, kc *kubeclient.KubeClient, clusterName str
 	}
 
 	for _, i := range service.Status.LoadBalancer.Ingress {
+		fmt.Printf("HOSTNAME=%s, IP=%s", i.Hostname, i.IP)
+		fmt.Printf("service status=%v", service.Status)
 		if i.Hostname != "" || i.IP != "" {
 			return nil
 		}
